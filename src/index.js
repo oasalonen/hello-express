8989d822b97484f0f16beae72b86d73a66a83a86
Olli Salonen <oasalonen@gmail.com>
2020-04-29 19:03:55 +0200

    Publish the app during build
    Add an OpenAPI spec and some paths

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/boom', (req, res) => {
+    null.boom();
+    res.send('ok');
+});
+
+// Serve static content
+const staticPath = path.join(__dirname, '..', 'static');
+app.use(express.static(staticPath));
+
+app.listen(port, () => console.log(`Hello Express listening at http://localhost:${port}`));
